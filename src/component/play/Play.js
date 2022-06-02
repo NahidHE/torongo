3aa69b4be3f29077dbb7c3c17f0996715cfe92a1
Nahid Hasan <NahidPundra.CSE@gmail.com>
2022-06-02 11:02:49 +0600

    final

--- src/component/play/Play.js
+++ src/component/play/Play.js
@@ -11,7 +11,7 @@ export default function Play(props) {
         src={url}
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
     </div>
